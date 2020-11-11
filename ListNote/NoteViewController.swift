ce119e736ffac7984063fa540ff7f80d43e0c068
Joan <joanchaupis@hotmail.com>
2020-11-10 20:50:51 -0500

    iOS note

---
+++ ListNote/NoteViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NoteViewController.swift
+//  ListNote
+//
+//  Created by Joan Paredes on 11/9/20.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note : String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+       
+    }
+    
+
+ 
+}
